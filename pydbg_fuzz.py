aaf29b9b19b8bc249301ea8d5c93992faa6eea72
c2ypt1c <c2ypt1c@gmail.com>
2015-09-23 22:04:51 -0700

    modified pydbg_fuzz to use class object parameter

--- pydbg_fuzz.py
+++ pydbg_fuzz.py
@@ -8,43 +8,53 @@ import threading
 import subprocess
 import time
 
-logdir = "C:\\crashbin\\"
-#gflags_path = "C:\\Program Files\\Windows Kits\\8.1\\Debuggers\\x86\\gflags.exe"
+gflags_path = "C:\\Program Files\\Windows Kits\\8.1\\Debuggers\\x86\\gflags.exe"
 # or place gflags in current directory
-gflags_path = "gflags.exe"
+#gflags_path = "gflags.exe"
+
 
 class pydbg_fuzz:
-    def __init__(self, target_exe, fuzz_file=None, timeout=1):
 
+    ################################################################################
+    # @param opts: a struct/class object containing the fuzzing options:
+    #       opts.exe
+    #       opts.fuzzed_file
+    #       opts.timeout
+    ###
+    def __init__(self, opts):
         #FIXME: create log directory if it doesn't exist
-
-        self.target_exe = target_exe
-        self.fuzz_file = fuzz_file
-
-        self.exe = target_exe[target_exe.rfind("\\")+1:]
-
+        self.opts = opts
         self.t = threading.Thread(target=self.fuzz)
         self.t.start()
 
-        self.timer = threading.Timer(timeout, self.kill_proc)
+        self.timer = threading.Timer(self.opts.timeout, self.kill_proc)
         self.timer.start()
 
         # wait until thread terminates
         self.t.join()
     
+    ################################################################################
+    # Terminates the current debugged process
+    ###
     def kill_proc(self):
         self.timer.cancel()
         self.dbg.terminate_process()
 
+    ################################################################################
+    # Creates backup files when a crash occurs
+    ###
     def create_backup(self, timestamp):
         #FIXME: create crash directory for each crash
         # and write backups into it
-        fin = open(self.fuzz_file, 'rb')
-        fout = open("%s%s_poc.bak" % (logdir, timestamp), 'wb')
+        fin = open(self.opts.fuzzed_file, 'rb')
+        fout = open("%s%s_poc.bak" % (self.opts.log_dir, timestamp), 'wb')
         fout.write(fin.read())
         fin.close()
         fout.close()
 
+    ################################################################################
+    # Handles access violations
+    ###
     def av_handler(self, dbg):
         # cancel process termination timer
         self.timer.cancel()
@@ -57,7 +67,7 @@ class pydbg_fuzz:
         crash_bin = crash_binning()
         crash_bin.record_crash(dbg)
 
-        f = open("%s%s_dbg.txt" % (logdir, timestamp), 'w')
+        f = open("%s%s_dbg.txt" % (self.opts.log_dir, timestamp), 'w')
         f.write(crash_bin.crash_synopsis())
         f.close()
 
@@ -72,10 +82,13 @@ class pydbg_fuzz:
     #def resume(self):
         #self.dbg.resume_all_threads()
 
+    ################################################################################
+    # Invokes the pydbg
+    ###
     def fuzz(self):
         self.dbg = pydbg()
         self.dbg.set_callback(EXCEPTION_ACCESS_VIOLATION, self.av_handler)
-        self.dbg.load(self.target_exe, self.fuzz_file)
+        self.dbg.load(self.opts.exe, self.opts.fuzzed_file)
         self.dbg.run()
 
 # public funtions
